cfbd16ad0f67727a50df44aaca340b8c89d3a5d7
smasuzawa <koubousei@gmail.com>
2017-05-22 18:45:03 +0900

    20170522分

--- js/common.js
+++ js/common.js
@@ -5,8 +5,9 @@ $(function(){
 
 //アコーディオン1
 $(".acrdBox1 .acrdBtn").click(function(){
-	$(this).parents(".acrdBox1").find(".acrdPart").slideToggle();
-	$(this).parents(".acrdBox1").toggleClass("opn");
+	var parents = $(this).parents(".acrdBox1");
+	parents.find(".acrdPart").slideToggle();
+	parents.toggleClass("opn");
 	return false;
 });//.acrdBox1 .acrdBtn ClkEnd
 
@@ -17,17 +18,32 @@ $(".tabBox1 .tabPart > li").click(function(){
 	if($(this).hasClass("show")){
 		return false;
 	}else{
-		var No = $(this).attr("class").replace("tabNo", "");
 		var parents = $(this).parents(".tabBox1");
+		var No = $(this).attr("class").replace("tabNo", "");
 		parents.find(".tabPart > li").removeClass("show");
 		$(this).addClass("show");
+		parents.find(".tabConts > li").removeClass("show");
+		parents.find(".tabConts > li.tabContNo" + No).addClass("show");
 		return false;
 	}
 });//.tabBox1 .tabPart > li ClkEnd
 
 
 
-
+/*----- toTopボタン -----*/
+//表示非表示
+var sclNum = 0;
+$(window).scroll(function(){
+	sclNum = $(window).scrollTop();
+	if(sclNum > 700){	$(".toTop").fadeIn("fast");}
+	else{	$(".toTop").fadeOut("fast");}
+});
+//clickによるスクロール
+$(".toTop").click(function(){
+	$('body, html').animate({scrollTop:0}, 400, 'swing');
+	return false;
+});
+/*----- /toTopボタン -----*/
 
 
 });//Fnc End
